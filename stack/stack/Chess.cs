7f901c3cf4ce4e81f59452c053d8a87519aded30
dkchoi <ehdqkwk92@gmail.com>
2023-04-08 17:01:03 +0900

    체스판 다시칠하기 solve

--- stack/stack/Chess.cs
+++ stack/stack/Chess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,14 @@ namespace algoritm
         {
         }
 
-        public Pair(T first, U second)
+        public Pair(T Col, U Row)
         {
-            this.First = first;
-            this.Second = second;
+            this.Col = Col;
+            this.Row = Row;
         }
 
-        public T First { get; set; }
-        public U Second { get; set; }
+        public T Col { get; set; }
+        public U Row { get; set; }
     };
     public class Chess
     {
@@ -27,30 +28,44 @@ namespace algoritm
         public static StreamWriter sw = new(Console.OpenStandardOutput());
         static void Main(string[] args)
         {
-            var whiteBoard = new char[,] 
-            { 
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-            };
-            var blackBoard = new char[,]
-            {
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-                { 'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W' },
-                { 'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B' },
-            };
             string[] inputSizes = (sr.ReadLine() ?? "0 0").Split(" ");
             Pair<int, int> size = new(int.Parse(inputSizes[0]), int.Parse(inputSizes[1]));
+            var board = new string[50];
+            int result = int.MaxValue;
+            for(int i = 0; i < size.Col; ++i)
+            {
+                board[i] = sr.ReadLine() ?? string.Empty;
+            }
+
+            for(int col = 0; col <= size.Col - 8; ++col)
+            {
+                for(int row = 0; row <= size.Row - 8; ++row)
+                {
+                    var min = GetSolution(col, row, board);
+                    min = Math.Min(min, 64 - min);
+                    result = Math.Min(min, result);
+                }
+            }
+            sw.WriteLine(result);
+            sw.Close();
+            sr.Close();
+        }
+
+        private static int GetSolution(int col, int row, string[] board)
+        {
+            int cnt = 0;
+            for(int i = col; i < col + 8; ++i)
+            {
+                for(int j = row; j < row + 8; ++j)
+                {
+                    var c = (i + j) % 2 == 0 ? 'W' : 'B';
+                    if(c != board[i].ElementAt(j))
+                    {
+                        cnt++;
+                    }
+                }
+            }
+            return cnt;
         }
     }
 }
