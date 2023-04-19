aab0fe30215cdc728ce8c515d560771ad180cdbf
tobby <dkchoi@maxst.com>
2023-04-19 10:15:54 +0900

    백준 1436

---
+++ stack/stack/Baekjoon_1436.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoritm
+{
+    public class Baekjoon_1436
+    {
+        public static StreamReader sr = new(Console.OpenStandardInput());
+        public static StreamWriter sw = new(Console.OpenStandardOutput());
+        static void Main(string[] args)
+        {
+            int num = int.Parse(sr.ReadLine() ?? "0");
+            int result = 665;
+            int count = 0;
+            while (true)
+            {
+                if (IsApocalypse(result))
+                {
+                    count++;
+                    if (count == num)
+                    {
+                        break;
+                    }
+                }
+                result++;
+            }
+            sw.WriteLine(result);
+            sw.Close();
+        }
+
+        static bool IsApocalypse(int num)
+        {
+            int temp = num;
+            while(temp != 0)
+            {
+                if((temp % 1000) == 666)
+                {
+                    return true;
+                }
+                temp /= 10;
+            }
+
+            return false;
+        }
+    }
+}
